eac4bb4d8f33ba33e853a76ef974ce28f4ebe218
root <root@ubuntu-xyong.(none)>
2014-11-18 08:53:41 +0800

    add power.so

--- power/db/intest.sh
+++
@@ -1,5 +0,0 @@
-X=/mnt/freenas/servers-transfer/powercat-ftrace-test
-#./parse-for-all-funcgraph ftrace.data powercat-ftrace
-ruby EnterDynamic-Nexux5.rb . android-4.4.3 real arm-Nexus5-test
-
-ruby power_analyse.rb android-4.4.3 real arm-Nexus5-test
