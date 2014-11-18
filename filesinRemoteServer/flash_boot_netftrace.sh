bfe8cd44a3240b5bd21c0f93ecc50b85ae844715
root <root@ubuntu-xyong.(none)>
2014-11-18 10:30:37 +0800

    modify

--- filesinRemoteServer/flash_boot_netftrace.sh
+++ filesinRemoteServer/flash_boot_netftrace.sh
@@ -1,5 +1,7 @@
 PATH=$PATH:/home/wjbang/adt-bundle-linux-x86-20131030/sdk/platform-tools
+
+server_sh_location="/home/wjbang/ftrace/"
 adb reboot bootloader
-fastboot flash boot /home/wjbang/20141104-ftrace-stable-boot.img
+fastboot flash boot ${server_sh_location}/boot.img
 fastboot reboot
 sleep 15
--- filesinRemoteServer/kill-server.sh
+++ filesinRemoteServer/kill-server.sh
@@ -1,7 +1,8 @@
-pid=$(ps -ef | grep /home/wjbang/ftrace/server_tcp_onefile | awk '{print $2}')
+server_sh_location="/home/wjbang/ftrace/"
+pid=$(ps -ef | grep ${server_sh_location}/server_tcp_onefile | awk '{print $2}')
 kill $pid
 sleep 10
-pid=$(ps -ef | grep /home/wjbang/ftrace | awk '{print $2}')
+pid=$(ps -ef | grep ${server_sh_location} | awk '{print $2}')
 kill $pid
 pid=$(ps -ef | grep "sleep 9999" | awk '{print $2}')
 kill $pid
